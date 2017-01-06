f7348e249edea657152075520c91f116c09afd0d
Yu Zhou <junior147147@yahoo.com>
2017-01-06 09:54:17 -0800

    super basic js implementation

---
+++ udemy/js/js_stalker_exercise/stalker.js
@@ -0,0 +1,19 @@
+// *Ask for user's first & last name
+var firstname = prompt("Yo, What is your first name?");
+var lastname = prompt("Yo, What is your last name?");
+
+// *Ask for age
+var age = prompt("Yo, AGE?????")
+var day_living = age * 365
+
+// *Print first/last name
+alert("OK, so your name is " + firstname + " " + lastname)
+alert("AND I KNOW YOUR AGE IS "+ age)
+alert("You have lived " + day_living + " days")
+
+// *Console *
+console.log("Wow, I know the name now " + firstname + " " + lastname)
+console.log("Wow, I know the age now " + age)
+
+
+// *Print age
---
+++ udemy/js/script.js
@@ -0,0 +1,10 @@
+// Some Methods
+
+// Prompt the User for their names, store in a variable
+var name = prompt("What is your name?");
+
+//Annoy the user with their name shows on the screen
+alert("Ok," + name + ", You are a nice man! " + name)
+
+//Annoy myself in the console
+console.log("wow, that was really fake" + name)
