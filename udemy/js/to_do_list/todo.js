d8f38f7a90f72b9b6a9c2aebfef90773cd865cf6
Yu Zhou <junior147147@yahoo.com>
2017-01-08 14:23:20 -0800

    Add new functions & fix layout for todo

--- udemy/js/to_do_list/todo.js
+++ udemy/js/to_do_list/todo.js
@@ -1,19 +1,40 @@
-
-
-
+/************Main***********/
 // Make an array
-var todo_array = [];
+var todo_array = ["Buy a cup of tea"];
 // Prompt for command
 
 var input = prompt("What can I do for you?");
 
 while (input !== "quit") {
-  input = prompt("What can I do for you?");
-  if (input === "new") {
-      var new_todo = prompt("Ok, what activity would you like to add?");
-      todo_array.push(new_todo);
+  if (input === "list") {
+    listTodos();
+  } else if(input === "new") {
+    newTodos();
+  } else if (input === "delete") {
+    deleteTodos();
     }
-  else if (input === "list") {
-    console.log(todo_array);
-  }
+  input = prompt("What can I do for you?");
+}
+console.log("OK,I QUIT FOR YOUUUUU");
+
+
+/************Function definitions***********/
+function listTodos(){
+  console.log("**********");
+  todo_array.forEach(function(todo, index){
+    console.log(index + ": " + todo);
+  });
+  console.log("**********");
+}
+
+function newTodos(){
+  var new_todo = prompt("Ok, what activity would you like to add?");
+  todo_array.push(new_todo);
+}
+
+function deleteTodos(){
+  var array_index = prompt("Which Todo are you willing to erase?")
+  // todo_array.pop(array_index);
+  todo_array.splice(array_index, 1)
+  console.log("Deleted Todo");
 }
