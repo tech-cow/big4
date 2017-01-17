aee463a41217e49ebb7dc33515dff0fe7c52b407
Yu Zhou <junior147147@yahoo.com>
2017-01-17 12:43:28 -0800

    partials update

--- udemy/node/EJSDemo/app.js
+++ udemy/node/EJSDemo/app.js
@@ -1,21 +1,38 @@
 var express = require("express");
 var app = express();
 
-//render
-app.get("/",function(req,res){
-  res.render("home.ejs");
-});
-
-app.get("/fallinlovewith/:thing",function(req,res){
-  var thing = req.params.thing;
-  res.render("love.ejs",{thingVar: thing});
-});
-
 
 // Including css
 app.use(express.static(__dirname + '/public'));
 
+//set the default file to be ejs
+app.set("view engine","ejs");
 
-app.listen(3000, function(){
-  console.log("Server is listening!");
+
+//render
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love", {
+        thingVar: thing
+    });
+});
+
+
+app.get("/posts", function(req, res){
+    var posts = [
+      {title: "1", author: "yz"},
+      {title: "2", author: "ms"},
+      {title: "3", author: "bd"}
+    ];
+    res.render("posts",{posts: posts});
+});
+
+
+
+app.listen(3000, function() {
+    console.log("Server is listening!");
 });
