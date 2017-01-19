af65509522de393c41ef51b7a0cea1f13f56339f
Yu Zhou <junior147147@yahoo.com>
2017-01-18 17:29:25 -0800

    ...

--- udemy/node/api/movie_app/app.js
+++ udemy/node/api/movie_app/app.js
@@ -4,14 +4,22 @@ var request = require("request")
 
 /******* Configuration *******/
 app.set("view engine","ejs");
+app.use(express.static(__dirname + '/public'));
 
 
 app.get("/",function(req,res){
-    res.send("Hello");
+  res.render("search");
 });
 
+// app.get("/search",function(req,res){
+    // res.render("search");
+// });
+
 app.get("/results", function(req,res){
-    request("http://www.omdbapi.com/?s=california", function(error,response,body){
+    var query = req.query.movieName;
+    var url = "http://www.omdbapi.com/?s=" + query;
+
+    request(url, function(error,response,body){
         if(!error && response.statusCode == 200){
           //body is an string, parsing turn string to an object
           var data = JSON.parse(body)
