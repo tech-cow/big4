083958133b94126c5b10b2dcf56b2d047571f906
Yu Zhou <junior147147@yahoo.com>
2017-02-04 18:16:15 -0800

    Done with merge sort

--- algorithm_AKA_谷歌我来了/basic/arraySort/mergeSort.py
+++ algorithm_AKA_谷歌我来了/basic/arraySort/mergeSort.py
@@ -8,55 +8,45 @@
 # 把Array对半分解，直到每个Array里面只有一个数，
 # 然后在开始对这些subset进行排序，排序完了进行合并，直到合并完成
 #=======================================================================
-def mergeSort(alist):
-    #Array给拆了，先得把它拆解掉
-    leftArray = []
-    rightArray = []
-
-    mid = len(alist)/2
-    #把比中间值小的，弄到leftArray
-    for i in range(0,mid):
-        leftArray.append(alist[i])
-        alist.pop(i)
-
-    #把比中间值小的，弄到rightArray
-    for j in range(mid,len(alist)):
-        rightArray.append(alist[i])
-        alist.pop(i)
-
-    while(len(leftArray) > 1):
-        mergeSort(leftArray)
-
-    while(len(rightArray) > 1):
-        mergeSort(rightArray)
-
-    # print leftAlist
-    # print rightArray
-
-
-
-def merge(leftAlist,rightAlist,newAlist):
+def merge(leftAlist,rightAlist):
+    result = []
     #当两边的Array都还有数的时候，互相对比，然后把最小值放进新的Array
     while(len(leftAlist) > 0 and len(rightAlist) > 0):
         if leftAlist[0] < rightAlist[0]:
-            newAlist.append(leftAlist[0])
+            result.append(leftAlist[0])
             leftAlist.pop(0)
         else:
-            newAlist.append(rightAlist[0])
+            result.append(rightAlist[0])
             rightAlist.pop(0)
 
     #当一边已经没有数的时候，把另一边所有的数都放进信的Array
     while(len(leftAlist) > 0):
-        newAlist.append(leftAlist[0])
+        result.append(leftAlist[0])
         leftAlist.pop(0)
 
     while(len(rightAlist) > 0):
-        newAlist.append(rightAlist[0])
+        result.append(rightAlist[0])
         rightAlist.pop(0)
 
-    return newAlist
+    return result
+
+def mergeSort(alist):
+    if len(alist) <= 1:
+        return alist
+
+    #Array给拆了，先得把它拆解掉
+    mid = len(alist)/2
+    #因为Recursive了，所以mergeSort这个方程会进行到底，知道list里只剩下一个值
+    left = mergeSort(alist[:mid])
+    right = mergeSort(alist[mid:])
+    return merge(left,right)
+
+
+
 
 
 
 #Test
 if __name__ == "__main__":
+    A = [3,2,1,4,6,5,66,111,77,88,99]
+    print mergeSort(A)
