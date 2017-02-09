ecd206625dc57aa5e5f47dcb872badb788e85486
Yu Zhou <junior147147@yahoo.com>
2017-02-09 11:18:28 -0800

    finishing 3 different order print

--- algorithm_AKA_谷歌我来了/graphs_and_trees/binarySearchTree.py
+++ algorithm_AKA_谷歌我来了/graphs_and_trees/binarySearchTree.py
@@ -3,36 +3,27 @@
 #=======================================================================
 #  Author: Yu Zhou
 #  Title: Binary Search Tree
-#
-# Node Class诠释：
-#
-# Binary Search Tree ADT
-#   Insert:
-#       步骤一：创建新的Node
-#
-# Reference:
-# 1. 视频： https://www.youtube.com/watch?v=oSWTXtMglKE
+#  Reference:
+# 1. https://www.youtube.com/watch?v=oSWTXtMglKE  #大致
+# 2. https://www.youtube.com/watch?v=gm8DUJJhmY4  #PreOrder,InOrder，和PostOrder讲得好
 #=======================================================================
 class Node(object):
     def __init__(self, data):
         self.data = data
-        self.leftChild = None
-        self.rightChild = None
+        self.leftChild = None  #左护法
+        self.rightChild = None   #右护法
 
     def insert(self,newData):
-        #Edge Case: 若value相等，怎么处理word天？
         if newData == self.data:
             return False
-
         elif newData < self.data:
             if self.leftChild == None:
-                self.leftChild == Node(self.data)
+                self.leftChild = Node(newData)
             else:
                 self.leftChild.insert(newData)
-
         else:
             if self.rightChild == None:
-                self.rightChild == Node(self.data)
+                self.rightChild = Node(newData)
             else:
                 self.rightChild.insert(newData)
 
@@ -51,9 +42,40 @@ class Node(object):
                 return self.rightChild.findValue(newData)
 
 
+    #PreOrder: <Root><Left><Right>
+    def preOrder(self):
+        print self.data  #先打印Root
+        if self.leftChild:  #设置Recursive的条件，self.leftChild = (self.leftChild != None)
+            self.leftChild.preOrder()  #再打印左边，然后不停的按照<Root><Left>打印
+        if self.rightChild:
+            self.rightChild.preOrder()   #当<Left>成为Null的时候，向上返回，开始打印<Right>
+
+
+    #InOrder: <Left><Root><Right>
+    def inOrder(self):
+        if self.leftChild:
+            self.leftChild.inOrder()
+        print self.data #打印leftChild最底层的数
+        if self.rightChild:
+            self.rightChild.inOrder()
+
+    #PostOrder: <Left><Right><Root>
+    def postOrder(self):
+        if self.leftChild:
+            self.leftChild.postOrder()
+        if self.rightChild:
+            self.rightChild.postOrder()
+        print self.data
 
 if __name__ == '__main__':
-    testNode = Node(5)
-    testNode.insert(6)
-    testNode.insert(6)
-    print testNode.findValue(6)
+
+    a = [3,8,9,1,2]
+    n = Node(5)
+    for i in range(len(a)):
+        n.insert(a[i])
+
+    # testNode.printMin()
+    # testNode.printMax()
+    # n.preOrder()
+    # n.inOrder()
+    # n.postOrder()
