f9dd47b45587269c6ff635630731f4ba296f9f01
Yu Zhou <junior147147@yahoo.com>
2017-02-09 21:49:28 -0800

    huge change, new Class Tree, things make way more sense now

--- algorithm_AKA_谷歌我来了/graphs_and_trees/binarySearchTree.py
+++ algorithm_AKA_谷歌我来了/graphs_and_trees/binarySearchTree.py
@@ -3,79 +3,133 @@
 #=======================================================================
 #  Author: Yu Zhou
 #  Title: Binary Search Tree
+#
 #  Reference:
 # 1. https://www.youtube.com/watch?v=oSWTXtMglKE  #大致
 # 2. https://www.youtube.com/watch?v=gm8DUJJhmY4  #PreOrder,InOrder，和PostOrder讲得好
 #=======================================================================
 class Node(object):
-    def __init__(self, data):
+    def __init__(self, data, left=None, right=None, parent=None):
+        self.left = left  #左护法
         self.data = data
-        self.leftChild = None  #左护法
-        self.rightChild = None   #右护法
+        self.right = right   #右护法
 
-    def insert(self,newData):
-        if newData == self.data:
+
+class Tree(object):
+    def __init__(self):
+        #先设置一个空的Node，之后再Insert的时候往里面放数
+        self.root = Node(None)
+
+        #test purpose lists
+        self.preOrderList = []
+        self.inOrderList = []
+        self.postOrderList = []
+
+    def search(self, newData):
+        if self.root.data == None:
             return False
-        elif newData < self.data:
-            if self.leftChild == None:
-                self.leftChild = Node(newData)
-            else:
-                self.leftChild.insert(newData)
         else:
-            if self.rightChild == None:
-                self.rightChild = Node(newData)
-            else:
-                self.rightChild.insert(newData)
+            return self.__search(self.root, newData)
 
-    def findValue(self,newData):
-        if newData == self.data:
+    def __search(self, root, newData):
+        if root.data == newData:
             return True
-        elif newData < self.data:
-            if self.leftChild == None:
-                return False
-            else:
-                return self.leftChild.findValue(newData)
         else:
-            if self.rightChild == None:
-                return False
+            if newData < root.data:
+                if root.left:
+                    return self.__search(root.left, newData)
+                else:
+                    return False
             else:
-                return self.rightChild.findValue(newData)
+                if root.right:
+                    return self.__search(root.right, newData)
+                else:
+                    return False
+
+    def insert(self, newData):
+        if self.root.data == None:
+            self.root.data = newData
+        else:
+            self.__insert(self.root, newData)
+
+    def __insert(self, root, newData):
+        if newData < root.data:
+            if root.left:
+                self.__insert(root.left, newData)
+            else:
+                root.left = Node(newData)
+        elif newData > root.data:
+            if root.right:
+                self.__insert(root.right, newData)
+            else:
+                root.right = Node(newData)
+        else:
+            print newData,' is Already in the list'
+
+    def inOrderTraversal(self):
+        if self.root:
+            self.__inOrderTraversal(self.root)
+
+    def __inOrderTraversal(self,root):
+        if root.left:
+            self.__inOrderTraversal(root.left)
+        #Append list & Print
+        self.inOrderList.append(root.data)
+        print root.data
+        if root.right:
+            self.__inOrderTraversal(root.right)
+
+    def preOrderTraversal(self):
+        if self.root:
+            self.__preOrderTraversal(self.root)
+
+    def __preOrderTraversal(self,root):
+        #Append list & Print
+        self.preOrderList.append(root.data)
+        print root.data
+        if root.left:
+            self.__preOrderTraversal(root.left)
+        if root.right:
+            self.__preOrderTraversal(root.right)
 
 
-    #PreOrder: <Root><Left><Right>
-    def preOrder(self):
-        print self.data  #先打印Root
-        if self.leftChild:  #设置Recursive的条件，self.leftChild = (self.leftChild != None)
-            self.leftChild.preOrder()  #再打印左边，然后不停的按照<Root><Left>打印
-        if self.rightChild:
-            self.rightChild.preOrder()   #当<Left>成为Null的时候，向上返回，开始打印<Right>
+    def postOrderTraversal(self):
+        if self.root:
+            self.__postOrderTraversal(self.root)
+
+    def __postOrderTraversal(self,root):
+        if root.left:
+            self.__postOrderTraversal(root.left)
+        if root.right:
+            self.__postOrderTraversal(root.right)
+        #Append list & Print
+        self.postOrderList.append(root.data)
+        print root.data
 
 
-    #InOrder: <Left><Root><Right>
-    def inOrder(self):
-        if self.leftChild:
-            self.leftChild.inOrder()
-        print self.data #打印leftChild最底层的数
-        if self.rightChild:
-            self.rightChild.inOrder()
 
-    #PostOrder: <Left><Right><Root>
-    def postOrder(self):
-        if self.leftChild:
-            self.leftChild.postOrder()
-        if self.rightChild:
-            self.rightChild.postOrder()
-        print self.data
 
+
+
+
+#test
 if __name__ == '__main__':
 
-    a = [3,8,9,1,2]
-    n = Node(5)
-    for i in range(len(a)):
-        n.insert(a[i])
+    bst = Tree()
+    bst.insert(5)
+    bst.insert(3)
+    bst.insert(4)
+    bst.insert(1)
+    bst.insert(7)
 
-    # testNode.printMin()
-    # testNode.printMax()
-    # n.preOrder()
-    # n.inOrder()
-    # n.postOrder()
+    print("pre order")
+    bst.preOrderTraversal()
+    # self.assertEqual(bst.preOrderList, [5,3,1,4,7])
+
+    print("in order")
+    bst.inOrderTraversal()
+    # self.assertEqual(bst.inOrderList, [1,3,4,5,7])
+
+    print("post order")
+    bst.postOrderTraversal()
+    # self.assertEqual(bt.postOrderList, [1,3,4,5,7])
