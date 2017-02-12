e761b71ee6c9ad49b3d14ba1f842949fea2482e0
Yu Zhou <junior147147@yahoo.com>
2017-02-11 16:31:07 -0800

    finishing BST

--- algorithm_AKA_谷歌我来了/graphs_and_trees/test.py
+++ algorithm_AKA_谷歌我来了/graphs_and_trees/test.py
@@ -14,7 +14,7 @@ class Node(object):
         self.left = left  #左护法
         self.data = data
         self.right = right   #右护法
-        self.parent = parent
+
 
 class Tree(object):
     def __init__(self):
@@ -46,22 +46,21 @@ class Tree(object):
         if self.root.data == None:
             self.root.data = newData
         else:
-            self.__insert(self.root, newData)
+            self.__insert(self.root.parent, self.root, newData)
 
-    def __insert(self, root, newData):
+    def __insert(self, parent, root, newData):
         if newData < root.data:
             if root.left:
-                self.__insert(root.left, newData)
+                self.__insert(root, root.left, newData)
             else:
                 root.left = Node(newData)
-                root.left.parent = root
-
+                root.parent.left.left = Node(newData)
         elif newData > root.data:
             if root.right:
-                self.__insert(root.right, newData)
+                self.__insert(root ,root.right, newData)
             else:
                 root.right = Node(newData)
-                root.right.parent = root
+                root.parent.right.left = Node(newData)
 
         else:
             print newData,' is Already in the list'
@@ -100,15 +99,6 @@ class Tree(object):
             self.__postOrderTraversal(root.right)
         print root.data
 
-
-
-#=======================================================================
-# delete
-# 该方程里面解决所有base case的比较
-# 基于只有最初始的Root，和最初始的Root.left以及Root.right
-# 若当输入的数，不符合Root，Root.left以及Root.right其中的值
-# 将调用另外一个比较方程__delete
-#=======================================================================
     def delete(self,newData):
         if self.root.data == None:
             print 'The tree is empty'
@@ -129,46 +119,27 @@ class Tree(object):
         elif newData == root.data:
             #case 1, root has no child
             if root.left is None and root.right is None:
-                if root.parent == None:  #先确认老大他爹存不存在，不存在的话，这个就是起始的Root,然后将其删除
-                    root = None
-                elif root.parent.left == root: #要是现在这个Root Node是其他Node的Child，我们不删除Root本身，因为Pass by value，传递不上去
-                    root.parent.left = None    #我们把Root.parent的Child删掉，这样可以向上传递
-                else: #elif root.parent.right == root:
-                    root.parent.right = None
-                print 'case 1: deleting', newData
-                return True
-
+                root = None
             #case 2, root has one child (left)
             elif root.left is not None and root.right is None:
-                root.data = root.left.data      #root.data = root.parent.left.data
+                root.data = root.left.data
                 #假如左边的Child还有Child的话：
                 root.right = root.left.right
                 root.left = root.left.left
-                print('case 2: deleting', newData)
-                return True
 
             #case 3, root has one child (right)
             elif root.right is not None and root.left is None:
                 root.data = root.right.data
                 #假如右边的Child还有Child的话：
-                #if root.right.left :
-                root.left = root.right.left
-                root.right = root.right.right
-                print 'case 3: deleting', newData
-                return True
+                root.left = root.left.left
+                root.right = root.left.riht
 
             #case 4, root has both children,
             # find the smallest node in the right subtree, and swipe value
             # delete smallest node in the right subtree
             else:
-                minRightNode = self.__minValueToRightSubtree(root.right)
-                root.data = minRightNode.data
-                if minRightNode == root.right:
-                    root.right = root.right.right
-                else:
-                    minRightNode.parent.left = None
-                print 'case 4: deleting', newData
-                return True
+                root.data = self.__minValueToRightSubtree(root.right).data
+                self.__deleteMinValueToRightSubtree(root.right)
         else:
             print "Can't find this number"
 
@@ -178,19 +149,39 @@ class Tree(object):
         else:
             return self.__minValueToRightSubtree(root.left)
 
+    def __deleteMinValueToRightSubtree(self, root):
+        if root.left is None:
+            root = None
+            return root
+        else:
+            self.__minValueToRightSubtree(root.left)
+
+
+
 #test
 if __name__ == '__main__':
 
     bst = Tree()
-    bst.insert(6)
-    bst.insert(4)
-    bst.insert(8)
-    bst.insert(7)
-    bst.insert(1)
     bst.insert(5)
-    bst.insert(9)
-    bst.insert(10)
+    bst.insert(3)
+    # bst.insert(4)
+    # bst.insert(1)
+    bst.insert(7)
+
+    # print("pre order")
+    # bst.preOrderTraversal()
+    # self.assertEqual(bst.preOrderList, [5,3,1,4,7])
+
     print("in order")
     bst.inOrderTraversal()
-    bst.delete(8)
+    bst.delete(3)
+    print("after deletion: in order")
     bst.inOrderTraversal()
+
+
+
+    # self.assertEqual(bst.inOrderList, [1,3,4,5,7])
+
+    # print("post order")
+    # bst.postOrderTraversal()
+    # self.assertEqual(bt.postOrderList, [1,3,4,5,7])
